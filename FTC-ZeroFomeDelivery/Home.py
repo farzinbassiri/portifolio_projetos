cc08ab72a5ac1a014725232ed74d2017592ac33b
farzinbassiri <95175232+farzinbassiri@users.noreply.github.com>
2023-06-26 17:51:40 -0300

    Update Home.py
    #Correção do caminho da base de dados para ser compativel com o streamlit:
    df_raw = pd.read_csv('./dataset/zomato.csv')

--- FTC-ZeroFomeDelivery/Home.py
+++ FTC-ZeroFomeDelivery/Home.py
@@ -10,7 +10,8 @@ import streamlit.components.v1 as components
 
 # carregando a base de dados
 # dados obtidos no site: https://www.kaggle.com/datasets/akashram/zomato-restaurants-autoupdated-dataset?resource=download&select=zomato.csv
-df_raw = pd.read_csv('dataset\\zomato.csv')
+#Correção do caminho da base de dados para ser compativel com o streamlit:
+df_raw = pd.read_csv('./dataset/zomato.csv')
 
 df = data_wrangling(df_raw)
 
