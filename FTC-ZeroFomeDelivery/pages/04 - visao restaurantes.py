2b51d943e0ad1fc296facbaa733ebe816991ad17
farzinbassiri <95175232+farzinbassiri@users.noreply.github.com>
2023-06-26 17:48:57 -0300

    Update 04 - visao restaurantes.py
    #Correção do caminho da base de dados para ser compativel com o streamlit:
    df_raw = pd.read_csv('./dataset/zomato.csv')

--- FTC-ZeroFomeDelivery/pages/04 - visao restaurantes.py
+++ FTC-ZeroFomeDelivery/pages/04 - visao restaurantes.py
@@ -8,7 +8,8 @@ import streamlit as st
 
 # carregando a base de dados
 # dados obtidos no site: https://www.kaggle.com/datasets/akashram/zomato-restaurants-autoupdated-dataset?resource=download&select=zomato.csv
-df_raw = pd.read_csv('dataset\\zomato.csv')
+#Correção do caminho da base de dados para ser compativel com o streamlit:
+df_raw = pd.read_csv('./dataset/zomato.csv')
 df = data_wrangling(df_raw)
 st.set_page_config(page_title= 'Visão Cidades', layout='wide')
 #--------------------------------------------------------------
