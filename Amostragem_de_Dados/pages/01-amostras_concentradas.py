74ab7337851bcc3e436e5d121f31e3782a55cd4d
Farzin Bassiri <95175232+farzinbassiri@users.noreply.github.com>
2023-12-23 23:11:56 -0300

    Update 01-amostras_concentradas.py

--- Amostragem_de_Dados/pages/01-amostras_concentradas.py
+++ Amostragem_de_Dados/pages/01-amostras_concentradas.py
@@ -36,9 +36,9 @@ st.set_page_config(page_title= 'Aprendizado de Máquina - Classificação - KNN'
 # exibe o path de trabalho desse notebook
 # print(pwd)
 try: #caminho para  uso local
-    df_raw = pd.read_csv('Dataset/trackLog_civic_dados_limpos.csv', delimiter=';', low_memory=False, usecols=[1,2,3,4,5,6])
+    df_raw = pd.read_csv('Amostragem_de_Dados/Dataset/trackLog_civic_dados_limpos.csv', delimiter=';', low_memory=False, usecols=[1,2,3,4,5,6])
 except: #caminho para Streamlit
-    df_raw = pd.read_csv('..\\Dataset\\trackLog_civic_dados_limpos.csv', delimiter=';', low_memory=False, usecols=[1,2,3,4,5,6])
+    df_raw = pd.read_csv('Dataset\\trackLog_civic_dados_limpos.csv', delimiter=';', low_memory=False, usecols=[1,2,3,4,5,6])
 
 df_raw= df_raw.convert_dtypes()
 
