6505c7746693a4d225fad45602fc643758510b42
Farzin Bassiri <95175232+farzinbassiri@users.noreply.github.com>
2024-03-10 14:59:54 -0300

    Add files via upload

---
+++ Testes_Geoespaciais/Home.py
@@ -0,0 +1,44 @@
+
+# importanto módulos e funções criadas para limpeza e pré-tratamento dos dados
+#from data_wrangling_modules import *
+# carregando demais biliotecas
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+import streamlit.components.v1 as components
+import folium
+from folium.plugins import MarkerCluster
+from streamlit_folium import folium_static
+#from PIL import Image
+
+st.set_page_config(page_title= 'AMC alunos', layout='wide')
+
+#--------------------------------------------------------------
+#                    Barra Lateral    
+#--------------------------------------------------------------
+st.markdown('# Aprendizado de Geolocalização')
+st.markdown('## Marcadores com a localização de endereços')
+st.markdown("""___""")
+
+
+max_width = 1028
+max_height = 702
+
+df = pd.read_excel('dataset/dataset_DW.xlsx', sheet_name='Dados_tratados')
+print(df.shape)
+
+
+start_location = [df.loc[:, 'lat'].median(), df.loc[:,'lon'].median()]
+mapa = folium.Map(location= start_location, min_zoom = 0, zoom_start= 13, control_scale=False)
+marker_cluster = MarkerCluster().add_to(mapa)
+#adiciona os pontos geográficos ao mapa
+for index, location_info in df.iterrows():
+    if (location_info['lon']!=0) & (location_info['lat']!=0):
+        popup_text = 'Logradouro: ' + str(location_info['Endereco'])
+        folium.Marker( [location_info['lat'], location_info['lon']],
+                        popup=location_info['Endereco'], 
+                        icon=folium.Icon(icon='')
+                     ).add_to(marker_cluster)
+
+
+folium_static(mapa, width = max_width, height = max_height)
