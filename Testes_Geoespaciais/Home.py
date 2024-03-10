460d2ab89627b68a340a91a68e50f23807aec946
Farzin Bassiri <95175232+farzinbassiri@users.noreply.github.com>
2024-03-10 19:06:51 -0300

    Update Home.py

--- Testes_Geoespaciais/Home.py
+++ Testes_Geoespaciais/Home.py
@@ -25,13 +25,7 @@ max_height = 702
 
 
 # Arquivos de entrada
-try: #caminho para Streamlit
-    df = pd.read_excel('Testes_Geoespaciais/dataset_DW.xlsx', sheet_name='Dados_tratados')
-except: #caminho para uso local
-    df = pd.read_excel('dataset\\dataset_DW.xlsx', sheet_name='Dados_tratados')
-
-print(df.shape)
-
+df = pd.read_excel('Testes_Geoespaciais\\dataset_DW.xlsx', sheet_name='Dados_tratados')
 
 start_location = [df.loc[:, 'lat'].median(), df.loc[:,'lon'].median()]
 mapa = folium.Map(location= start_location, min_zoom = 0, zoom_start= 13, control_scale=False)
