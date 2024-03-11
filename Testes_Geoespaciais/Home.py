986f48765f32ee00454259afed4019dc233e5269
Farzin Bassiri <95175232+farzinbassiri@users.noreply.github.com>
2024-03-10 21:45:18 -0300

    Add files via upload

--- Testes_Geoespaciais/Home.py
+++ Testes_Geoespaciais/Home.py
@@ -10,22 +10,25 @@ import folium
 from folium.plugins import MarkerCluster
 from streamlit_folium import folium_static
 
-st.set_page_config(page_title= 'Geolocalização', layout='wide')
+st.set_page_config(page_title= 'Regiões de atendimento', layout='wide')
 
 #--------------------------------------------------------------
 #                    Barra Lateral    
 #--------------------------------------------------------------
-st.markdown('# Aprendizado de Geolocalização')
-st.markdown('## Marcadores com a localização de endereços')
+st.markdown('# Mapa para apoio à tomada de decisões ')
 st.markdown("""___""")
 
-
-max_width = 1028
-max_height = 702
+# definição global de tamanho dos gráficos
+max_width = 1024
+max_height = 700
 
 
 # Arquivos de entrada
-df = pd.read_excel('Testes_Geoespaciais/dataset_DW.xlsx', sheet_name='Dados_tratados')
+try: #caminho para Streamlit
+    df = pd.read_excel('Testes_Geoespaciais/dataset_DW.xlsx', sheet_name='Dados_tratados')
+except: #caminho para uso local
+	df = pd.read_excel('dataset/dataset_DW.xlsx', sheet_name='Dados_tratados')
+
 
 start_location = [df.loc[:, 'lat'].median(), df.loc[:,'lon'].median()]
 mapa = folium.Map(location= start_location, min_zoom = 0, zoom_start= 13, control_scale=False)
@@ -39,5 +42,19 @@ for index, location_info in df.iterrows():
                         icon=folium.Icon(icon='')
                      ).add_to(marker_cluster)
 
+		
+#criando abas
+#Abas com as diferentes métricas 
 
-folium_static(mapa, width = max_width, height = max_height)
+tab1, tab2, tab3 = st.tabs(['Ruas com atendimentos', 'Ruas com colaboradores residentes', '-'])
+
+# separa os códigos para cada aba
+# todo código que estiber em 'with tab1 vai aparecer naquela aba
+
+with tab1: 
+	with st.container():
+		folium_static(mapa, width = max_width, height = max_height)
+		
+		
+with tab2:
+	st.markdown('# Em desenvolvimento')
