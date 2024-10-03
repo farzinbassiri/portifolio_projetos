0b1d5ad2916a0d9ad94c796026c27b72be21d512
Farzin Bassiri <95175232+farzinbassiri@users.noreply.github.com>
2024-10-03 19:46:59 -0300

    update home.py
    Novos mapas adicionados

--- Testes_Geoespaciais/Home.py
+++ Testes_Geoespaciais/Home.py
@@ -3,12 +3,16 @@
 #from data_wrangling_modules import *
 # carregando demais biliotecas
 import pandas as pd
+import math
 import plotly.express as px
 import streamlit as st
 import streamlit.components.v1 as components
 import folium
 from folium.plugins import MarkerCluster
 from streamlit_folium import folium_static
+
+import os.path
+
 
 st.set_page_config(page_title= 'Regiões de atendimento', layout='wide')
 
@@ -30,36 +34,131 @@ except: #caminho para uso local
 	df = pd.read_excel('dataset/dataset_DW.xlsx', sheet_name='Dados_tratados')
 
 
-start_location = [df.loc[:, 'lat'].median(), df.loc[:,'lon'].median()]
-mapa = folium.Map(location= start_location, min_zoom = 0, zoom_start= 13, control_scale=False)
-marker_cluster = MarkerCluster().add_to(mapa)
-#adiciona os pontos geográficos ao mapa
-for index, location_info in df.iterrows():
-    if (location_info['lon']!=0) & (location_info['lat']!=0):
-        popup_text = 'Logradouro: ' + str(location_info['Endereco'])
-        folium.Marker( [location_info['lat'], location_info['lon']],
-                        popup=location_info['Endereco'], 
-                        icon=folium.Icon(icon='')
-                     ).add_to(marker_cluster)
 
-		
+
+
+
+# funções
+def angle(p1, p2):
+    """Calcula o ângulo entre dois pontos em relação ao eixo x."""
+    x_diff = p2[0] - p1[0]
+    y_diff = p2[1] - p1[1]
+    return math.atan2(y_diff, x_diff)
+
+def organize_points(points):
+    """Organiza os pontos para formar um polígono sem cruzamentos."""
+    # Encontrar o ponto mais à esquerda
+    start_point = min(points, key=lambda p: p[1])
+
+    # Ordenar os pontos por ângulo em relação ao ponto inicial
+    sorted_points = sorted(points, key=lambda p: angle(start_point, p))
+    sorted_points.insert(0, start_point)
+
+    return sorted_points
+
+
+# busca dados de mapas especiais
+try:
+    df_mapa_1 = pd.read_excel('Testes_Geoespaciais/coordenadas_v1.xlsx', sheet_name='mapa_1')
+except:
+    df_mapa_1 = pd.read_excel(os.path.abspath(os.getcwd()) +'/dataset/coordenadas_v1.xlsx', sheet_name='mapa_1')
+
+try: 
+    df_mapa_2 = pd.read_excel('Testes_Geoespaciais/coordenadas_v1.xlsx', sheet_name='mapa_2')       
+
+except:
+    df_mapa_2 = pd.read_excel(os.path.abspath(os.getcwd()) + '/dataset/coordenadas_v1.xlsx', sheet_name='mapa_2') 
+# acerta o formato
+df_mapa_1.lat = df_mapa_1.lat.astype('float64')
+df_mapa_1.lon = df_mapa_1.lon.astype('float64')
+df_mapa_2.lat = df_mapa_2.lat.astype('float64')
+df_mapa_2.lon = df_mapa_2.lon.astype('float64')
+
+# Lista de coordenadas (latitude, longitude)
+coordinates_m1 = df_mapa_1.loc[:,['lat','lon']].values
+coordinates_m2 = df_mapa_2.loc[:,['lat','lon']].values
+
+# Organizar os pontos
+org_coordinates_m1 = organize_points(coordinates_m1)
+org_coordinates_m2 = organize_points(coordinates_m2)
+
+
+
+
 #criando abas
 #Abas com as diferentes métricas 
 
-tab1, tab2, tab3 = st.tabs(['Ruas com atendimentos', 'Ruas com colaboradores residentes', 'Zona de influência'])
+tab1, tab2, tab3 = st.tabs(['Ruas com atendimentos', 'Roteiro de Visitação', 'Zona de influência'])
 
 # separa os códigos para cada aba
 # todo código que estiber em 'with tab1 vai aparecer naquela aba
 
 with tab1: 
-	with st.container():
-		folium_static(mapa)
+    with st.container():
+        start_location = [df.loc[:, 'lat'].median(), df.loc[:,'lon'].median()]
+        mapa = folium.Map(location= start_location, min_zoom = 0, zoom_start= 13, control_scale=False)
+        marker_cluster = MarkerCluster().add_to(mapa)
+        #adiciona os pontos geográficos ao mapa
+        for index, location_info in df.iterrows():
+            if (location_info['lon']!=0) & (location_info['lat']!=0):
+                popup_text = 'Logradouro: ' + str(location_info['Endereco'])
+                folium.Marker( [location_info['lat'], location_info['lon']],
+                                popup=location_info['Endereco'], 
+                                icon=folium.Icon(icon='')
+                             ).add_to(marker_cluster)
+        folium_static(mapa)
 		
 		
 with tab2:
-	st.markdown('# Em desenvolvimento')
-	
-	
+    start_location = [df_mapa_1.loc[:, 'lat'].median(), df_mapa_1.loc[:,'lon'].median()]
+    m1 = folium.Map(location= start_location, min_zoom = 0, zoom_start= 15, control_scale=True)
+    marker_cluster = MarkerCluster().add_to(m1)
+    
+    folium.Polygon(locations=org_coordinates_m1,
+                   color="blue",
+                   weight=2,
+                   fill_color="blue",
+                   fill_opacity=0.2,
+                   fill=True).add_to(m1)
+    
+    #adiciona os pontos geográficos ao mapa
+    for index, location_info in df_mapa_1.iterrows():
+        if (location_info['lon']!=0) & (location_info['lat']!=0):
+            popup_text = 'Descrição: ' + str(location_info['Descricao'])
+            folium.Marker( [location_info['lat'], location_info['lon']],
+                            popup=location_info['Descricao'], 
+                            icon=folium.Icon(icon='')
+                         ).add_to(marker_cluster)
+
+
+    start_location = [df_mapa_2.loc[:, 'lat'].median(), df_mapa_2.loc[:,'lon'].median()]
+    m2 = folium.Map(location= start_location, min_zoom = 0, zoom_start= 14, control_scale=True)
+    marker_cluster = MarkerCluster().add_to(m2)
+    
+    folium.Polygon(locations=org_coordinates_m2,
+                   color="blue",
+                   weight=2,
+                   fill_color="blue",
+                   fill_opacity=0.2,
+                   fill=True).add_to(m2)
+    
+    #adiciona os pontos geográficos ao mapa
+    for index, location_info in df_mapa_2.iterrows():
+        if (location_info['lon']!=0) & (location_info['lat']!=0):
+            popup_text = 'Descrição: ' + str(location_info['Descricao'])
+            folium.Marker( [location_info['lat'], location_info['lon']],
+                            popup=location_info['Descricao'], 
+                            icon=folium.Icon(icon='')
+                         ).add_to(marker_cluster)
+    
+    with st.container():
+    	folium_static(m1)
+    with st.container():
+        folium_static(m2)
+
+
+
+
 with tab3:
 	st.markdown('# Em desenvolvimento')
 	st.markdown('## A *zona de influência* é compreendida pela soma das regiões com atendimentos e as regiões com colaboradores residentes, formando a zona de influência positiva')
